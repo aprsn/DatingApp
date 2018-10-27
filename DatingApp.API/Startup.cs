c3c55ea6fd468472b9d71aeda544ecca78fd044b
Alper San <alpersan@Alper-MacBook-Pro.local>
2018-10-27 21:46:48 +0300

    End of section 11

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -41,6 +41,7 @@ namespace DatingApp.API
                     opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;  //Ignores the error!
                 });
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper();
             services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
